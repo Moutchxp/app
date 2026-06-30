f597d8b28f0d13bce8cf05d2a5649492b7ab7f05
Moutchxp <a.jorel@sansvisavis.com>
2026-06-30 20:14:47 +0200

    chore(dev): sous-domaine localtunnel fixe dans allowedDevOrigins (accès iPhone HTTPS stable)

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: any = {
   /* config options here */
-  allowedDevOrigins: ['hungry-poets-glow.loca.lt', '192.168.1.164'],
+  allowedDevOrigins: ['svv-arno-test.loca.lt', '192.168.1.164'],
   devIndicators: false,
 };
 
