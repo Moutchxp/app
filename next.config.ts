5d7cc24e7b283c76720e1a28f5fd801bee097924
Moutchxp <a.jorel@sansvisavis.com>
2026-06-30 20:32:00 +0200

    chore(dev): remplace localtunnel par cloudflared + wildcard *.trycloudflare.com dans allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: any = {
   /* config options here */
-  allowedDevOrigins: ['svv-arno-test.loca.lt', '192.168.1.164'],
+  allowedDevOrigins: ['*.trycloudflare.com', 'assembled-mitchell-construct-charms.trycloudflare.com', '192.168.1.164'],
   devIndicators: false,
 };
 
